9be55f0c57856e0c626365c20beb04498f9ce818
Joy Udom <judom.2011@gmail.com>
2021-03-27 07:38:18 +0000

    new  exercise

---
+++ Array.java
@@ -0,0 +1,33 @@
+package chapterFour;
+
+public class Array {
+    public static void main(String[] args) {
+       int a;
+//        int [] array = new int[5];
+//
+//
+//            int frequency [0] = 10;
+//            frequency[1] = 20;
+//            frequency[2] = 21;
+//            frequency[3] = 50;
+//            frequency[4] = 89;
+//        for (a = 0; a <= frequency.length; a++) {
+//
+//        int counter;
+//
+//        for (counter = 0; counter < array.length; counter++)
+//        System.out.print(" " + a);
+
+//        java.util.Scanner input = new java.util.Scanner(System.in);
+//        double[] myList = new double[10];
+//        System.out.print("Enter " + myList.length + " values: ");
+//
+//        for (int i = 0; i < myList.length; i++) {
+//            myList[i] = Math.random() * 100;
+//        }
+
+    }
+
+
+
+}
