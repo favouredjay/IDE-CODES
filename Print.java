b022e86ae1ebbbacee8f1f0e759d2fef4abb9a19
Joy Udom <judom.2011@gmail.com>
2021-04-11 00:41:47 +0100

    chapter four

---
+++ Print.java
@@ -0,0 +1,9 @@
+package chapterFour;
+
+public class Print {
+    public static void main(String[] args) {
+        System.out.print(" 1 ");
+        System.out.print(" 2 ");
+        System.out.printf(" 1 " + " 2 ");
+    }
+}
